4ff57c96b3a79461dc19df61513b8ca01ed1bfd8
jorshi <j.m.shier@qmul.ac.uk>
2023-04-09 15:27:54 +0100

    Add a dilated residual block

--- mridangam/models.py
+++ mridangam/models.py
@@ -28,3 +28,38 @@ class MLP(torch.nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
+
+
+def _get_activation(activation: str):
+    return getattr(torch.nn, activation)()
+
+
+class DilatedResidualConvolution(torch.nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        dilation: int,
+        activation: str = "GELU",
+    ) -> None:
+        super().__init__()
+
+        self.norm = torch.nn.BatchNorm1d(in_channels)
+        self.convolution = torch.nn.Conv1d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            dilation=dilation,
+            padding="same",
+        )
+        self.activation = _get_activation(activation)
+        self.residual = torch.nn.Conv1d(in_channels, out_channels, 1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        assert x.dim() == 3
+        assert x.shape[1] == self.convolution.in_channels
+        y = self.norm(x)
+        y = self.convolution(y)
+        y = self.activation(y)
+        return y + self.residual(x)
